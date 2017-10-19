c0bac0205e59e8c2de3a6ea689db1b391198de05
nishina <n.toshi.1.014@gmail.com>
2017-10-20 03:55:20 +0900

    add eslintrc

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-export class Hello extends React.Component {
-  render (){
-    return <h1>Hello!</h1>
-  }
-}
+import React from 'react';
+
+const Hello = () => (
+  <h1>Hello!</h1>
+);
+
+export default Hello;
